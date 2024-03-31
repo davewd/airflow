d81001f904c4bb51fe2f5bb863cf95d67fe981fc
David Dawson <davedawson.co@gmail.com>
2024-03-31 14:16:12 +1100

    feat(python) add init files & minor runner update

--- python_codebase/dwdrun/runner.py
+++ python_codebase/dwdrun/runner.py
@@ -1,3 +1,12 @@
+__author__ = "David Dawson"
+__copyright__ = "Copyright 2020, David Dawson"
+__credits__ = ["David Dawson"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Dave Dawson"
+__email__ = "davedawson.co@gmail.com"
+__status__ = "Production"
+
 import datetime
 import argparse
 import importlib
@@ -6,12 +15,11 @@ def main(args):
     arg_module = args.module
     arg_module_import = importlib.import_module(arg_module)
     arg_module_import.main(args)
-
     # Your code to use the runtime argument goes here
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse arguments for runtime.')
-    parser.add_argument("module", default="data_aquisition.market_data.ecb.fx_api")
+    parser.add_argument("module", default="python_codebase.data_aquisition.market_data.ecb.fx_api")
     parser.add_argument("run_date", default=datetime.date(2024,1,1))
     parser.add_argument('--file', type=str, help='Specify the runtime for the file.')
     args = parser.parse_args()
