c448ba75ba96f0a8ded605e2a8c5c083b306a331
David Dawson <davedawson.co@gmail.com>
2024-04-01 21:46:45 +1100

    feat(runner) get arg parsing working

--- python_codebase/dwdrun/runner.py
+++ python_codebase/dwdrun/runner.py
@@ -11,16 +11,27 @@ import datetime
 import argparse
 import importlib
 
+
 def main(args):
-    arg_module = args.module
+    arg_module = args.jobModule
     arg_module_import = importlib.import_module(arg_module)
     arg_module_import.main(args)
     # Your code to use the runtime argument goes here
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Parse arguments for runtime.')
-    parser.add_argument("module", default="python_codebase.data_aquisition.market_data.ecb.fx_api")
-    parser.add_argument("run_date", default=datetime.date(2024,1,1))
-    parser.add_argument('--file', type=str, help='Specify the runtime for the file.')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Parse arguments to injection into python layer"
+    )
+    parser.add_argument(
+        "--jobModule",
+        "-j",
+        default="python_codebase.data_aquisition.market_data.ecb.fx_api",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--runDate", "-d", default=datetime.date(2024, 1, 1), type=str, required=True
+    )
     args = parser.parse_args()
     main(args)
