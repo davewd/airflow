7aaf10010ecb394ef39aff7ee64f9564b0c5bacc
David Dawson <davedawson.co@gmail.com>
2024-04-07 13:12:35 +1000

    feat(python codebase) enable imports from python

--- python_codebase/dwdrun/runner.py
+++ python_codebase/dwdrun/runner.py
@@ -11,6 +11,7 @@ import datetime
 import argparse
 import importlib
 import logging
+import dynamic_import_lib
 
 logger = logging.getLogger(__name__)
 
@@ -18,16 +19,14 @@ logger = logging.getLogger(__name__)
 def main(args):
     arg_module = args.jobModule
     run_datetime = args.runDate
-    logger.info(f"Starting Module: {arg_module} for Run Date/Time: {run_datetime}")
-    arg_module_import = importlib.import_module(arg_module)
+    logger.info(f"Starting Module: {arg_module} for run Date/Time: {run_datetime}")
+    arg_module_import = importlib.import_module(arg_module, "...")
     arg_module_import.main(args)
     # Your code to use the runtime argument goes here
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Parse arguments to injection into python layer"
-    )
+    parser = argparse.ArgumentParser(description="Parse arguments to injection into python layer")
     parser.add_argument(
         "--jobModule",
         "-j",
@@ -35,8 +34,8 @@ if __name__ == "__main__":
         type=str,
         required=True,
     )
-    parser.add_argument(
-        "--runDate", "-d", default=datetime.date(2024, 1, 1), type=str, required=True
-    )
+    parser.add_argument("--runDate", "-d", default=datetime.date(2024, 1, 1), type=str, required=True)
     args = parser.parse_args()
     main(args)
+
+# https://stackoverflow.com/questions/72395188/how-to-dynamically-import-module-from-a-relative-path-with-python-importlib
