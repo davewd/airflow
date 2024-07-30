ebf5fb8264c400a36f0c27a666d1f8470c661ffa
David Dawson <davedawson.co@gmail.com>
2024-07-30 23:16:54 +1000

    feat(read the docs) initial revision of files

---
+++ python_codebase/lib/docs/functions.py
@@ -0,0 +1,20 @@
+"""
+This module contains constants and enumerations used throughout the project.
+
+@author: David Dawson
+@copyright: Copyright 2020, David Dawson
+@credits: David Dawson
+@license: GPL
+@version: 1.0.1
+@maintainer: Dave Dawson
+@email: davedawson.co@gmail.com
+@status: Production
+"""
+
+from enum import Enum
+
+
+# Define enumerations
+class DocumentType(Enum):
+    CYBER = 1
+    SOLUTION = 2
---
+++ python_codebase/lib/docs/models.py
@@ -0,0 +1,43 @@
+__author__ = "David Dawson"
+__copyright__ = "Copyright 2020, David Dawson"
+__credits__ = ["David Dawson"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Dave Dawson"
+__email__ = "davedawson.co@gmail.com"
+__status__ = "Production"
+
+
+class MarkDownGenerator:
+    def __init__(self) -> None:
+        return None
+
+    def generate_markdown_table(self):
+        return ""
+
+
+class ApplicationDefinition(MarkDownGenerator):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def generate_documentation(self) -> List[str]:
+        """
+        Generate documentation for the application.
+
+        This function generates a list of strings, where each string represents a
+        section of the documentation. The sections are ordered in a specific way,
+        and each section contains relevant information about the application.
+
+        Parameters:
+        None
+
+        Returns:
+        List[str]: A list of strings representing the documentation sections.
+        """
+        return []
+
+    def generate_markdown_documentation(self):
+        return ""
+
+    def generate_html_documentation(self):
+        return ""
