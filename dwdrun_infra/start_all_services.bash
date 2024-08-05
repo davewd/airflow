da27230a90c928f8d9d50451ca32bb63fe8e4cd1
David Dawson <davedawson.co@gmail.com>
2024-08-05 21:51:50 +1000

    feat(docker compose) ensure we check docker network and auto create on start up

--- dwdrun_infra/start_all_services.bash
+++ dwdrun_infra/start_all_services.bash
@@ -5,6 +5,17 @@ bash ./build_infra.sh
 cd ..
 echo Starting AIRFLOW UI webserver
 
+if [ ! "$(docker network ls | grep dwdrunnetwork)" ]; then
+    echo "Creating dwdrunnetwork network ..."
+    docker network create dwdrunnetwork
+else
+    echo "dwdrunnetwork network exists."
+fi
+
+#Create Server certificate and key
+# Run this in the certs folder.  last updated : 5Aug25
+#openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj "/CN=localhost" -nodes
+
 #docker network create dwdrunnetwork
 cd ./airflow_infra_config/
 docker-compose up
